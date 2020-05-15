ded97033a3ab8a20e3abb49297a40bb3fd1a68b2
Kaniah Dunn <kaniahdunn@gmail.com>
2020-05-14 21:29:27 -0400

    questions from April Daily Challenge

--- 1.js
+++ 1.js
@@ -26,6 +26,7 @@ const twoSum = function (nums, target) {
     }
 };
 
+//Single Number 
 /**Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 
 Note:
@@ -56,6 +57,7 @@ var singleNumber = function (nums) {
     return num;
 };
 
+//Happy Number
 /*Write an algorithm to determine if a number is "happy".
 
 A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
@@ -71,10 +73,10 @@ Explanation:
 12 + 02 + 02 = 1
 
 */
-/**
- * @param {number} n
- * @return {boolean}
- */
+// 
+//  @param {number} n
+//  @return {boolean}
+//  
 var isHappy = function (n, seen) {
     let sum = 0;
     if (seen === undefined) {
@@ -93,3 +95,32 @@ var isHappy = function (n, seen) {
     seen.push(sum);
     return isHappy(sum, seen);
 };
+
+//Maximum Subarray 
+//Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// Example:
+
+// Input: [-2,1,-3,4,-1,2,1,-5,4],
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+// Follow up:
+
+// If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+
+//Move Zeroes
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Example:
+
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Note:
+
+// You must do this in-place without making a copy of the array.
+// Minimize the total number of operations.
+
+// Hint #1 : In-place means we should not be allocating any space for extra array. But we are allowed to modify the existing array. However, as a first step, try coming up with a solution that makes use of additional space. For this problem as well, first apply the idea discussed using an additional array and the in-place solution will pop up eventually.
+
+// Hint #2 : A two-pointer approach could be helpful here. The idea would be to have one pointer for iterating the array and another pointer that just works on the non-zero elements of the array.
